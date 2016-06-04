42368bc4a718f2c060d59478e35cc98deafcc75b
Emilien Sanchez <emilien@Emiliens-MacBook-Pro.local>
2016-06-04 19:25:11 +0200

    yolo

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,25 @@ router.get('/', function(req, res, next) {
   	res.render('skillmatch/index', { title: 'Express', users:users,categories:categories,myskills:myskills });
 });
 
+router.get('/search', function(req, res, next) {
+	var users = data.users;
+	var categories = data.categories;
+	var myskills = data.myskills;
+  	res.render('skillmatch/search', { title: 'Express', users:users,categories:categories,myskills:myskills });
+});
+
+router.get('/network', function(req, res, next) {
+	var users = data.users;
+	var categories = data.categories;
+	var myskills = data.myskills;
+  	res.render('skillmatch/network', { title: 'Express', users:users,categories:categories,myskills:myskills });
+});
+
+router.get('/integration', function(req, res, next) {
+	var users = data.users;
+	var categories = data.categories;
+	var myskills = data.myskills;
+  	res.render('skillmatch/integrations', { title: 'Express', users:users,categories:categories,myskills:myskills });
+});
+
 module.exports = router;
