5f757ab074817f3369ec541644dc5ef9bab2667a
Emilien Sanchez <emilien@Emiliens-MacBook-Pro.local>
2016-06-04 22:40:11 +0200

    adding slack

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var data = require('../data/data-skillmatch.js')
+var request = require('request')
 var router = express.Router();
 
 /* GET home page. */
@@ -45,7 +46,37 @@ router.get('/integration', function(req, res, next) {
 	var users = data.users;
 	var categories = data.categories;
 	var myskills = data.myskills;
-  	res.render('skillmatch/integrations', { title: 'Express', users:users,categories:categories,myskills:myskills });
+	req.param('slack')
+	var hasSlack = req.param('slack')
+  	res.render('skillmatch/integrations', { title: 'Express', users:users,categories:categories,myskills:myskills,hasSlack:hasSlack });
+});
+
+router.get('/auth/slack', function(req, res, next) {
+	var code = req.param('code');
+	var url = 'https://slack.com/api/oauth.access';
+
+        var postData = {
+            "code": code,
+            "client_id": '42172077798.48238370917',
+            "client_secret": '058e5651cf1b469e2f783bf6c685f57a',
+            "redirect_uri": 'https://hacktheoffice-winners.herokuapp.com/skillmatch/auth/slack'
+        }
+        
+        url = url +'?client_id='+postData.client_id+'&client_secret='+postData.client_secret+'&code='+postData.code+'&redirect_uri='+postData.redirect_uri;
+        
+        var options = {
+            method: 'post',
+            url: url
+        }
+        
+        request(options, function(err, r, body) {
+            if (err) {
+                return err 
+            }
+            res.redirect('/skillmatch/integration?slack=true')
+        });
+
+  	
 });
 
 module.exports = router;
