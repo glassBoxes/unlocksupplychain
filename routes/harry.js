fe761f14cae1997be295529ddd3af4fd6e82d82a
Emilien Sanchez <emilien@Emiliens-MacBook-Pro.local>
2016-06-05 01:06:30 +0200

    test

---
+++ routes/harry.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var users = require('../data/data-serenity.js')
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('serenity/index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res, next) {
+	var user = {name:"henry"}
+  res.render('serenity/index', { title: 'Express'});
+});
+
+
+module.exports = router;
